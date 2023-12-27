1dcbb6876ff301e7f77f9212bbdc0b937a4eda91
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-12-26 22:20:03 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/luan/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/luan/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.luan.algafoodapi.domain.exception.EntidadeEmUsoException;
 import com.luan.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.luan.algafoodapi.domain.exception.NegocioException;
 
 @ControllerAdvice//dentro desse componente é possivel adicionar exceptions handlers
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -33,17 +34,6 @@ public class ApiExceptionHandler {
 				.build();
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
-	}
-	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-		ApiError apiError = ApiError.builder()
-				.dataHora(LocalDateTime.now())
-				.msg("O tipo de mídia não é aceito.")
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-				.body(apiError);
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
