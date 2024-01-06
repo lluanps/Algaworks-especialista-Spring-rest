75a97b665c18f502865d6b3832686a3288965b47
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-06 18:32:54 -0300

    customizando e resolvendo mensagens de validação com Resource Bundle

--- src/main/java/com/luan/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/luan/algafoodapi/domain/model/Restaurante.java
@@ -49,7 +49,7 @@ public class Restaurante {
 	private String nome;
 	
 	@NotNull 
-	@PositiveOrZero
+	@PositiveOrZero(message = "{TaxaFrete.invalida}")
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
