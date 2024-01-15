2d26c3d9f591618a6a338b54ddaa83dc30aa3893
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-15 18:21:51 -0300

    add metodo para excluir em forma de pagamento

--- src/main/java/com/luan/algafoodapi/api/controller/FormaPagamentoController.java
+++ src/main/java/com/luan/algafoodapi/api/controller/FormaPagamentoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,11 @@ public class FormaPagamentoController {
 		FormaPagamento formaPagamento = formaPagamentoInputDisassembler.toDomainObject(formaPagamentoInput);
 		formaPagamento = service.salvar(formaPagamento);
 		return formaPagamentoDTOAssembler.toModel(formaPagamento);
+	}
+	
+	@DeleteMapping("/{formaPagamentoId}")
+	public void excluir(@PathVariable Long formaPagamentoId) {
+		service.excluir(formaPagamentoId);
 	}
 
 }
