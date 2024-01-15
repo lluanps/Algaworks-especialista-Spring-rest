6d8f33e7d0a8eea7fcdcc50b60c9342625575ae9
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-15 17:30:40 -0300

    Correção em FormaPagamentoInputDisassembler

--- src/main/java/com/luan/algafoodapi/api/assembler/FormaPagamentoInputDisassembler.java
+++ src/main/java/com/luan/algafoodapi/api/assembler/FormaPagamentoInputDisassembler.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.luan.algafoodapi.api.model.input.EstadoInput;
+import com.luan.algafoodapi.api.model.input.FormaPagamentoInput;
 import com.luan.algafoodapi.domain.model.Estado;
+import com.luan.algafoodapi.domain.model.FormaPagamento;
 
 @Component
 public class FormaPagamentoInputDisassembler {
@@ -13,12 +15,12 @@ public class FormaPagamentoInputDisassembler {
 	@Autowired
 	private ModelMapper mapper;
 	
-	public Estado toDomainObject(EstadoInput estadoInput) {
-		return mapper.map(estadoInput, Estado.class);
+	public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput) {
+		return mapper.map(formaPagamentoInput, FormaPagamento.class);
 	}
 	
-	public void copyToDomainObject(EstadoInput estadoInput, Estado estado) {
-		mapper.map(estadoInput, estado);
+	public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+		mapper.map(formaPagamentoInput, formaPagamento);
 	}
 	
 }
