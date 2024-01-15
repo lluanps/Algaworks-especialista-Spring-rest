1d2dadb14efc69c6e4d2713bc364524929213cf8
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-15 17:56:08 -0300

    add metodo para buscar todos e busca por id em forma pagamento

--- src/main/java/com/luan/algafoodapi/api/controller/FormaPagamentoController.java
+++ src/main/java/com/luan/algafoodapi/api/controller/FormaPagamentoController.java
@@ -1,8 +1,12 @@
 package com.luan.algafoodapi.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,7 @@ import com.luan.algafoodapi.api.assembler.FormaPagamentoInputDisassembler;
 import com.luan.algafoodapi.api.model.FormaPagamentoDTO;
 import com.luan.algafoodapi.api.model.input.FormaPagamentoInput;
 import com.luan.algafoodapi.domain.model.FormaPagamento;
+import com.luan.algafoodapi.domain.repository.FormaPagamentoRepository;
 import com.luan.algafoodapi.domain.service.FormaPagamentoService;
 
 @RestController
@@ -23,10 +28,26 @@ public class FormaPagamentoController {
 	private FormaPagamentoService service;
 	
 	@Autowired
+	private FormaPagamentoRepository repository;
+	
+	@Autowired
 	private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
 	
 	@Autowired
 	private FormaPagamentoDTOAssembler formaPagamentoDTOAssembler;
+
+	@GetMapping
+	public List<FormaPagamentoDTO> listar() {
+		List<FormaPagamento> buscaTodos = repository.findAll();
+		
+		return formaPagamentoDTOAssembler.toCollectionDto(buscaTodos);
+	}
+	
+	@GetMapping("/{formaPagamentoId}")
+	public FormaPagamentoDTO buscarPorId(@PathVariable Long formaPagamentoId) {
+		FormaPagamento formaPagamento = service.buscaOuFalha(formaPagamentoId);
+		return formaPagamentoDTOAssembler.toModel(formaPagamento);
+	}
 	
 	@PostMapping
 	public FormaPagamentoDTO adicionar(@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
--- src/main/java/com/luan/algafoodapi/domain/service/FormaPagamentoService.java
+++ src/main/java/com/luan/algafoodapi/domain/service/FormaPagamentoService.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luan.algafoodapi.domain.exception.EstadoNaoEncontradaException;
 import com.luan.algafoodapi.domain.model.FormaPagamento;
 import com.luan.algafoodapi.domain.repository.FormaPagamentoRepository;
 
@@ -17,6 +18,11 @@ public class FormaPagamentoService {
 	@Transactional
 	public FormaPagamento salvar(FormaPagamento formaPagamento) {
 		return repository.save(formaPagamento);
+	}
+
+	public FormaPagamento buscaOuFalha(Long formaPagamentoId) {
+		return repository.findById(formaPagamentoId).orElseThrow(() -> new EstadoNaoEncontradaException(
+				String.format("Não foi encotrado nenhuma forma de pagamento com id: ", formaPagamentoId)));
 	}
 	
 }
