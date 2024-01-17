1b372174753884b595357c0d542521eace72d5ed
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-16 22:59:18 -0300

    criado exception GrupoNaoEncontradoException

---
+++ src/main/java/com/luan/algafoodapi/domain/exception/GrupoNaoEncontradoException.java
@@ -0,0 +1,14 @@
+package com.luan.algafoodapi.domain.exception;
+
+public class GrupoNaoEncontradoException extends EntidadeNaoEncontradaException {
+	private static final long serialVersionUID = 1L;
+
+	public GrupoNaoEncontradoException(String msg) {
+		super(msg);
+	}
+	
+	public GrupoNaoEncontradoException(Long grupoId) {
+		this(String.format("Não existe um cadastro de grupo com o código %d ", grupoId));
+	}
+
+}
