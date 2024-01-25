52628a737719bafc6278daa95123a69f66c61ee3
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-25 00:25:13 -0300

    Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/luan/algafoodapi/domain/model/FotoPedido.java
@@ -0,0 +1,31 @@
+package com.luan.algafoodapi.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Entity
+public class FotoPedido {
+
+	@Id
+	@Column(name = "produto_id")
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	private Produto produto;
+	
+	private String arquivo;
+	private String descricao;
+	private String contentType;
+	private Long tamanho;
+	
+}
