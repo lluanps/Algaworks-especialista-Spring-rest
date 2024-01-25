1ed4a2fd186855173aa473ad9ea037abd825171f
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-25 12:52:12 -0300

    Desafio: implementando endpoints de transição de status de pedidos

--- src/main/java/com/luan/algafoodapi/api/controller/FluxoPedidoController.java
+++ src/main/java/com/luan/algafoodapi/api/controller/FluxoPedidoController.java
@@ -23,6 +23,16 @@ public class FluxoPedidoController {
 		fluxoPedidoService.confirmar(pedidoId);
 	}
 	
+	@PutMapping("/entrega")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void entregar(@PathVariable Long pedidoId) {
+		fluxoPedidoService.entregar(pedidoId);
+	}
 	
+	@PutMapping("/cancelamento")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void cancelar(@PathVariable Long pedidoId) {
+		fluxoPedidoService.cancelar(pedidoId);
+	}
 	
 }
