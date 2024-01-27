c500d1a8a4726c0e608f60416c353c143c1e4819
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-27 12:54:26 -0300

    Checando media type ao servir arquivos de fotos

--- src/main/java/com/luan/algafoodapi/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/luan/algafoodapi/api/controller/RestauranteProdutoFotoController.java
@@ -1,14 +1,20 @@
 package com.luan.algafoodapi.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,12 +22,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.luan.algafoodapi.api.assembler.FotoProdutoDTOAssembler;
 import com.luan.algafoodapi.api.model.FotoProdutoDTO;
 import com.luan.algafoodapi.api.model.input.FotoProdutoInput;
+import com.luan.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.luan.algafoodapi.domain.model.FotoProduto;
 import com.luan.algafoodapi.domain.model.Produto;
 import com.luan.algafoodapi.domain.service.CatalogoFotoProdutoService;
+import com.luan.algafoodapi.domain.service.FotoStorageService;
 import com.luan.algafoodapi.domain.service.ProdutoService;
-
-import lombok.Getter;
 
 @RestController
 @RequestMapping("/restaurante/{restauranteId}/produtos/{produtoId}/foto")
@@ -35,6 +41,9 @@ public class RestauranteProdutoFotoController {
 
 	@Autowired
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private FotoStorageService fotoStorageService;
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
@@ -54,11 +63,41 @@ public class RestauranteProdutoFotoController {
 		return fotoProdutoDTOAssembler.toModel(fotoSalva);
 	}
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public FotoProdutoDTO buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		FotoProduto fotoProduto = catalogoFotoProdutoService.buscaOuFalha(restauranteId, produtoId);
 		
 		return fotoProdutoDTOAssembler.toModel(fotoProduto);
+	}
+	
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
+			@PathVariable Long produtoId, @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
+		try {
+			FotoProduto fotoProduto = catalogoFotoProdutoService.buscaOuFalha(restauranteId, produtoId);
+			
+			MediaType mediaTypeFoto = MediaType.parseMediaType(fotoProduto.getContentType());
+			List<MediaType> mediaTypesAceitas = MediaType.parseMediaTypes(acceptHeader);
+			
+			verificarCompatibilidadeMediaType(mediaTypeFoto, mediaTypesAceitas);
+			
+			InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+
+			return ResponseEntity.ok()
+					.contentType(mediaTypeFoto)
+					.body(new InputStreamResource(inputStream));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	private void verificarCompatibilidadeMediaType(MediaType mediaTypeFoto, List<MediaType> mediaTypesAceitas) throws HttpMediaTypeNotAcceptableException {
+		boolean compativel = mediaTypesAceitas.stream()
+					.anyMatch(mediaTypesAceita -> mediaTypesAceita.isCompatibleWith(mediaTypeFoto));
+		
+		if (!compativel) {
+			throw new HttpMediaTypeNotAcceptableException(mediaTypesAceitas);
+		}
 	}
 	
 }
