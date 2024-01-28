91e60b8a674f0c14bef9196dcda638ee431332fb
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-01-28 16:32:00 -0300

    S3 - Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/luan/algafoodapi/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/luan/algafoodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.luan.algafoodapi.core.storage.StorageProperties;
@@ -48,7 +49,18 @@ public class S3FotoStorageService implements FotoStorageService {
 
 	@Override
 	public void remover(String nomeArquivo) {
-		
+		try {
+			String caminhoArquvio = getCaminhoArquivo(nomeArquivo);
+			
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(),
+					caminhoArquvio);
+			
+			amazonS3.deleteObject(deleteObjectRequest);
+			
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+		}
 	}
 
 	@Override
