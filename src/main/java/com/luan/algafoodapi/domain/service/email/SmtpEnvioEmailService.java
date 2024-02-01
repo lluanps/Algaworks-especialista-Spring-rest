ef3097fb1d227c1bc695973ac2244a71425e33c2
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-01 12:46:03 -0300

    Usando o Apache FreeMarker para enviar email personalizado

--- src/main/java/com/luan/algafoodapi/domain/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/luan/algafoodapi/domain/service/email/SmtpEnvioEmailService.java
@@ -1,15 +1,24 @@
 package com.luan.algafoodapi.domain.service.email;
 
+import java.io.IOException;
+
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.luan.algafoodapi.core.email.EmailProperties;
 import com.luan.algafoodapi.domain.service.EnvioEmailService;
 import com.luan.algafoodapi.domain.service.email.exception.EmailException;
+
+import freemarker.core.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateNotFoundException;
 
 @Service
 public class SmtpEnvioEmailService implements EnvioEmailService {
@@ -19,22 +28,39 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	
 	@Autowired
 	private EmailProperties emailProperties;
+	
+	@Autowired
+	private Configuration freeMarkerConfig;
 
 	@Override
 	public void enviar(Mensagem mensagem) {
 		try {
+			String corpo = processarTemplate(mensagem);
+			
 			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 			
 			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
 			mimeMessageHelper.setFrom(emailProperties.getRemetente());
 			mimeMessageHelper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
 			mimeMessageHelper.setSubject(mensagem.getAssunto());
-			mimeMessageHelper.setText(mensagem.getCorpo(), true);
+			mimeMessageHelper.setText(corpo, true);
 			
 			javaMailSender.send(mimeMessage);
 			
 		} catch (Exception e) {
 			throw new EmailException("Não foi possivel enviar e-mail", e);
+		}
+	}
+	
+	private String processarTemplate(Mensagem mensagem) {
+		try {
+			Template template = freeMarkerConfig.getTemplate(mensagem.getCorpo());
+			
+			return FreeMarkerTemplateUtils.processTemplateIntoString(
+					template, mensagem.getVariaveis());
+			
+		} catch (Exception e) {
+			throw new EmailException("Não foi possivel montar o template do email", e);
 		}
 	}
 
