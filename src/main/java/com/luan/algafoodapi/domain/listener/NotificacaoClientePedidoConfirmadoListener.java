4f203311dacbda6fbba1cb8cadb90c1f4edba22f
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-02 13:15:10 -0300

    Observando e reagindo a Domain Events

---
+++ src/main/java/com/luan/algafoodapi/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -0,0 +1,33 @@
+package com.luan.algafoodapi.domain.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.luan.algafoodapi.domain.event.PedidoConfirmadoEvent;
+import com.luan.algafoodapi.domain.model.Pedido;
+import com.luan.algafoodapi.domain.service.EnvioEmailService;
+import com.luan.algafoodapi.domain.service.EnvioEmailService.Mensagem;
+
+//para criar um novo evento para adicionar uma nova regra, basta criar uma classe semelhante a essa q o evento sera disparado
+@Component
+public class NotificacaoClientePedidoConfirmadoListener {
+	
+	@Autowired
+	private EnvioEmailService envioEmail;
+
+	@EventListener
+	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+		Pedido pedido = event.getPedido();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("pedido-confirmado.html")
+				.variavel("pedido", pedido)
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmail.enviar(mensagem);
+	}
+
+}
