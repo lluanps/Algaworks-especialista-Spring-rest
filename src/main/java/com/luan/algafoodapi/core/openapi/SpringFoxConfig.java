ee1c0611773f27062cd413ec18db5ff6193167f7
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-07 10:04:42 -0300

    Gerando a documentação da API em HTML com Swagger UI e SpringFox

--- src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -2,20 +2,32 @@ package com.luan.algafoodapi.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
-  @Bean
-  public Docket apiDocket() {
-    return new Docket(DocumentationType.OAS_30)
-        .select()
-          .apis(RequestHandlerSelectors.any())// seleciona tudo
-          .build();
-  }
-  
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.apis(RequestHandlerSelectors.any())// seleciona tudo
+				.build();
+	}
+	
+	/* http://localhost:8080/swagger-ui/index.html */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+		
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
 }
