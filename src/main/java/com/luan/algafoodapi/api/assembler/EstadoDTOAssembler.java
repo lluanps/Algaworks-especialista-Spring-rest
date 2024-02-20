a50fc82bc4966ee78be689d2f19162c5425e4170
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-20 18:31:56 -0300

    Desafio: adicionando hypermedia nos recursos de estados

--- src/main/java/com/luan/algafoodapi/api/assembler/EstadoDTOAssembler.java
+++ src/main/java/com/luan/algafoodapi/api/assembler/EstadoDTOAssembler.java
@@ -5,25 +5,41 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import com.luan.algafoodapi.api.controller.EstadoController;
 import com.luan.algafoodapi.api.model.EstadoDTO;
 import com.luan.algafoodapi.domain.model.Estado;
 
 @Component
-public class EstadoDTOAssembler {
+public class EstadoDTOAssembler extends RepresentationModelAssemblerSupport<Estado, EstadoDTO> {
 
 	@Autowired
 	private ModelMapper mapper;
-	
-	public EstadoDTO toModel(Estado estado) {
-		return mapper.map(estado, EstadoDTO.class);
+
+	public EstadoDTOAssembler() {
+		super(EstadoController.class, EstadoDTO.class);
 	}
 	
-	public List<EstadoDTO> toCollectionDto(List<Estado> estados) {
-		return estados.stream()
-				.map(estado -> toModel(estado))
-				.collect(Collectors.toList());
+	@Override
+	public EstadoDTO toModel(Estado estado) {
+		EstadoDTO estadoDTO = createModelWithId(estado.getId(), estado);
+		
+		mapper.map(estado, estadoDTO);
+		
+		estadoDTO.add(WebMvcLinkBuilder.linkTo(
+				WebMvcLinkBuilder.methodOn(EstadoController.class).listar()).withRel("estados"));
+		
+		return estadoDTO;
+	}
+	
+	@Override
+	public CollectionModel<EstadoDTO> toCollectionModel(Iterable<? extends Estado> entities) {
+		return super.toCollectionModel(entities)
+				.add(WebMvcLinkBuilder.linkTo(EstadoController.class).withSelfRel());
 	}
 	
 }
--- src/main/java/com/luan/algafoodapi/api/model/EstadoDTO.java
+++ src/main/java/com/luan/algafoodapi/api/model/EstadoDTO.java
@@ -1,10 +1,12 @@
 package com.luan.algafoodapi.api.model;
 
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "estados")
 @Getter
 @Setter
 public class EstadoDTO extends RepresentationModel<EstadoDTO> {
