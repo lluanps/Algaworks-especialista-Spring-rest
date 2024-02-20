00eddb708bf4739bfbaf06b30cdf84ae5f6ca8cd
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-20 14:08:34 -0300

    fix - erro de sintaxe no metodo cidadeById da classe CidadeController

--- src/main/java/com/luan/algafoodapi/api/controller/CidadeController.java
+++ src/main/java/com/luan/algafoodapi/api/controller/CidadeController.java
@@ -57,7 +57,7 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Busca uma cidade po Id")
-	@GetMapping("/cidadeId")
+	@GetMapping("/{cidadeId}")
 	public CidadeDTO cidadeById(@ApiParam(value = "Id de uma cidade")
 	@PathVariable Long cidadeId) {
 		Cidade cidade = service.buscarOuFalhar(cidadeId);
