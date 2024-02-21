f8731958e6ad1255c0cc0b80dbfebc4b407a0c29
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-21 20:52:10 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -1,16 +1,24 @@
 package com.luan.algafoodapi.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,10 +33,26 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.select()
 //				.apis(RequestHandlerSelectors.any())// seleciona tudo
 				.apis(RequestHandlerSelectors.basePackage("com.luan.algafoodapi.api"))// seleciona pacote
+				.paths(PathSelectors.any())
 				.build()
+			.useDefaultResponseMessages(false)// desabilita codigo de Status 400
+			.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 			.apiInfo(apiInfo())
 			.tags(new Tag("Cidades", "Gerencia as cidades", 0));
 	}
+
+	private List<Response> globalGetResponseMessages() {
+		  return Arrays.asList(
+		      new ResponseBuilder()
+		          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+		          .description("Erro interno do Servidor")
+		          .build(),
+		      new ResponseBuilder()
+		          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+		          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+		          .build()
+		  );
+		}
 	
 	/* http://localhost:8080/swagger-ui/index.html */
 	@Override
