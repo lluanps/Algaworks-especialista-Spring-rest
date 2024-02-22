84b6874b7b164a294e8b73db0a6ab27d07088364
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-02-21 21:08:19 -0300

    Desafio: descrevendo códigos de status de respostas de forma global

--- src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/luan/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -37,8 +37,66 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.build()
 			.useDefaultResponseMessages(false)// desabilita codigo de Status 400
 			.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+			.globalResponses(HttpMethod.POST, globalPostResponseMessages())
+			.globalResponses(HttpMethod.PUT, globalPutResponseMessages())
+			.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
 			.apiInfo(apiInfo())
 			.tags(new Tag("Cidades", "Gerencia as cidades", 0));
+	}
+
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+					.description("Requisição inválida")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno do sistema")
+					.build()
+				);
+	}
+
+	private List<Response> globalPutResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+					.description("Requisição inválida")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno do sistema")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+					.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+					.description("Requisição recusado, pois o formato da requisição não é aceito")
+					.build()
+				);
+	}
+
+	private List<Response> globalPostResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno do sistema")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+					.description("Requisição inválida")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+					.description("ERecurso não possui representação que poderia ser aceita pelo consumidor")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+					.description("Requisição recusado, pois o formato da requisição não é aceito")
+					.build()
+				);
 	}
 
 	private List<Response> globalGetResponseMessages() {
